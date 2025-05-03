9c5f1a84259dd5b26463401fbf97b6a1f6b47706
Thiago Perin <thiagoperin2310@gmail.com>
2025-05-03 18:21:53 -0300

    backend: update app.module.ts to enable global access to envs in the project, and import the SpotifyModule

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,15 +1,18 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { ServeStaticModule } from '@nestjs/serve-static';
 import { join } from 'path';
+import { SpotifyModule } from './spotify/spotify.module';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
     ServeStaticModule.forRoot({
       rootPath: join(__dirname, '..', '..', 'frontend', 'out'),
-      exclude: ['/api*'],
+      exclude: ['/api*', '/spotify*'],
+      serveRoot: '/'
     }),
+    SpotifyModule,
   ],
-  controllers: [],
-  providers: [],
 })
 export class AppModule { }
