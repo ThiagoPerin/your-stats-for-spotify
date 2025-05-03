2b2677a4f5293dbf03e9467b3715d7098958ec48
Thiago Perin <thiagoperin2310@gmail.com>
2025-05-03 18:09:54 -0300

    frontend: adequações iniciais na tela inicial e callback

---
+++ frontend/src/app/callback/page.tsx
@@ -0,0 +1,27 @@
+"use client";
+import { Suspense } from "react";
+import { useSearchParams } from "next/navigation";
+
+function Teste() {
+    const searchParams = useSearchParams();
+    const token = searchParams.get("code");
+    console.log(token);
+
+    return (
+        <>
+            <div className="w-screen h-screen bg-black flex flex-col items-center justify-center">
+                <div className="text-white text-6xl">You are logged in !!!!</div>
+                <div className="text-white text-sm text-center">Your token is {token}</div>
+            </div>
+        </>
+    );
+}
+
+// Wrapping the component in Suspense with a fallback UI
+export default function TesteWrapper() {
+    return (
+        <Suspense fallback={<div>Loading...</div>}>
+            <Teste />
+        </Suspense>
+    );
+}
