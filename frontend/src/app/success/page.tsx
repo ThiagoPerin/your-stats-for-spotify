e5d1039d989ed1d1eefeaefd43983e2a1bcca018
Thiago Perin <thiagoperin2310@gmail.com>
2025-05-09 02:14:25 -0300

    frontend: update success page to get the session_id in the url

--- frontend/src/app/success/page.tsx
+++ frontend/src/app/success/page.tsx
@@ -4,20 +4,18 @@ import { useSearchParams } from "next/navigation";
 
 function Teste() {
     const searchParams = useSearchParams();
-    const token = searchParams.get("access_token") || "";
-    console.log(token);
+    const session_id = searchParams.get("session_id") || "";
 
     return (
         <>
             <div className="w-screen h-screen bg-black flex flex-col items-center justify-center">
                 <div className="text-white text-6xl">You are logged in !!!!</div>
-                <div className="text-white text-sm text-center">Your token is {token}</div>
+                <div className="text-white text-sm text-center">Your session_id is {session_id}</div>
             </div>
         </>
     );
 }
 
-// Wrapping the component in Suspense with a fallback UI
 export default function TesteWrapper() {
     return (
         <Suspense fallback={<div>Loading...</div>}>
