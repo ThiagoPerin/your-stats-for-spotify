26f86d8ae5055bd9d3347c4bd8b77e8503f07070
Thiago Perin <thiagoperin2310@gmail.com>
2025-05-09 02:06:57 -0300

    backend: imports redis module on spotify module

--- backend/src/spotify/spotify.module.ts
+++ backend/src/spotify/spotify.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { SpotifyController } from './spotify.controller';
 import { SpotifyService } from './spotify.service';
+import { RedisModule } from 'src/redis/redis.module';
 
 @Module({
+    imports: [RedisModule],
     controllers: [SpotifyController],
     providers: [SpotifyService],
 })
