ca02a85968df0005b95a0b6717fe4c66997bdfef
Thiago Perin <thiagoperin2310@gmail.com>
2025-05-10 12:42:10 -0300

    backend: adds two new endpoints for top artists and tracks in the api controller

--- backend/src/spotify/api/api.controller.ts
+++ backend/src/spotify/api/api.controller.ts
@@ -1,12 +1,28 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Controller, Get, Param, Query } from '@nestjs/common';
 import { ApiService } from './api.service';
 
 @Controller('spotify')
 export class ApiController {
     constructor(private readonly apiService: ApiService) { }
- 
-    @Get('user/:sessionId')
+
+    @Get('sessions/:sessionId/user')
     async getUser(@Param('sessionId') sessionId: string) {
         return this.apiService.getUserInfo(sessionId);
+    }
+
+    @Get('sessions/:sessionId/top/artists')
+    async getUserTopArtists(
+        @Param('sessionId') sessionId: string,
+        @Query('limit') limit: number = 10,
+    ) {
+        return this.apiService.getUserTopArtists(sessionId, limit);
+    }
+
+    @Get('sessions/:sessionId/top/tracks')
+    async getUserTopTracks(
+        @Param('sessionId') sessionId: string,
+        @Query('limit') limit: number = 10,
+    ) {
+        return this.apiService.getUserTopTracks(sessionId, limit);
     }
 }
