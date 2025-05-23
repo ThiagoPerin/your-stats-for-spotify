12e6a89cbc0332668385840df994f6a84b23a64e
Thiago Perin <thiagoperin2310@gmail.com>
2025-05-23 00:14:03 -0300

    feat: creata variables for spotify colors

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -6,10 +6,10 @@ export default function Home() {
   const router = useRouter()
   return (
     <>
-      <div className="w-screen h-screen bg-[#121212] flex flex-col items-center justify-center gap-10">
+      <div className="w-screen h-screen bg-spotify-black flex flex-col items-center justify-center gap-10">
         <img src="./Spotify_Primary_Logo_RGB_Green.png" alt="Spotify Logo"  className="h-32"/>
-        <h1 className="text-8xl font-bold text-[#ffffff]">Your Stats for Spotify</h1>
-        <Button className="bg-[#1ED760] text-[#121212]" onClick={() => router.push("/spotify/auth/login")}>Start This Adventure</Button>
+        <h1 className="text-8xl font-bold text-spotify-white">Your Stats for Spotify</h1>
+        <Button className="bg-spotify-green text-spotify-black" onClick={() => router.push("/spotify/auth/login")}>Start This Adventure</Button>
       </div>
     </>
   );
