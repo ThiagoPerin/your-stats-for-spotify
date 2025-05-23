823de99970226465ffd444fd694eb293339b3594
Thiago Perin <thiagoperin2310@gmail.com>
2025-05-23 00:07:27 -0300

    feat: test ui with some spotify colors and logo

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -6,9 +6,12 @@ export default function Home() {
   const router = useRouter()
   return (
     <>
-      <div className="w-screen h-screen bg-black flex items-center justify-center">
-        <Button className="bg-[#81b71a]" onClick={() => router.push("/spotify/auth/login")}>Start This Adventure</Button>
+      <div className="w-screen h-screen bg-[#121212] flex flex-col items-center justify-center gap-10">
+        <img src="./Spotify_Primary_Logo_RGB_Green.png" alt="Spotify Logo"  className="h-32"/>
+        <h1 className="text-8xl font-bold text-[#ffffff]">Your Stats for Spotify</h1>
+        <Button className="bg-[#1ED760] text-[#121212]" onClick={() => router.push("/spotify/auth/login")}>Start This Adventure</Button>
       </div>
     </>
   );
 }
+ 
